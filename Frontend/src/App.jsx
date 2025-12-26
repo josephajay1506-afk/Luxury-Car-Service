7e40d97c0a0cea44bc501bc667a489681e222d67
Ajay Joseph <josephajay.1506@gmail.com>
2025-12-26 18:45:28 +0530

    Day 1: Frontend setup with Tailwind CSS

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-900">
+      <h1 className="text-4xl font-bold text-yellow-400">
+        Luxury Car Service Platform
+      </h1>
+    </div>
+  );
+}
+
+export default App;
